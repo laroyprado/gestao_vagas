68e47e864b4f9418adfcd5a7e7a648a448356270
laroy prado <laroyprado@gmail.com>
2024-01-18 20:48:29 -0300

    feat: cria tabela no banco de dados e service de verificação de username

--- src/main/java/com/example/laroy/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/example/laroy/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,21 +1,26 @@
 package com.example.laroy.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 
-@Entity
+@Entity(name = "candidate")
 @Data
 public class CandidateEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
 
@@ -30,6 +35,9 @@ public class CandidateEntity {
     private String description;
     private String curriculum;
 
+
+    @CreationTimestamp 
+    private LocalDateTime createdAt;
     
 }
 
--- src/main/java/com/example/laroy/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/example/laroy/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,6 +1,7 @@
 package com.example.laroy.gestao_vagas.modules.candidate.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.laroy.gestao_vagas.exceptions.UserFoundException;
 import com.example.laroy.gestao_vagas.modules.candidate.CandidateEntity;
 import com.example.laroy.gestao_vagas.modules.candidate.CandidateRepository;
+import com.example.laroy.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 
 import jakarta.validation.Valid;
 
@@ -17,18 +19,20 @@ import jakarta.validation.Valid;
 public class CandidateController {
     
 
+
     @Autowired
-    private CandidateRepository candidateRepository;
+    private CreateCandidateUseCase createCandidateUseCase;
+
 
 
     @PostMapping("/")
-    public CandidateEntity create( @Valid @RequestBody CandidateEntity candidateEntity){
-        this.candidateRepository.
-        findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
-        .ifPresent((user)->{
-            throw new UserFoundException();
-        });
-        return this.candidateRepository.save(candidateEntity);
+    public ResponseEntity<Object> create( @Valid @RequestBody CandidateEntity candidateEntity){
+        try {
+            var result =  this.createCandidateUseCase.execute(candidateEntity);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
