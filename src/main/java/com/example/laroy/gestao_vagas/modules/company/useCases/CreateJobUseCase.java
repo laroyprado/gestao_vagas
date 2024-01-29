145ffbbd644deca24105d0ac345955476792a466
laroy prado <laroyprado@gmail.com>
2024-01-29 20:23:25 -0300

    feat: cria use case de criação de job

---
+++ src/main/java/com/example/laroy/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
@@ -0,0 +1,29 @@
+package com.example.laroy.gestao_vagas.modules.company.useCases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.laroy.gestao_vagas.modules.company.entities.JobEntity;
+import com.example.laroy.gestao_vagas.modules.company.repositories.JobRepository;
+
+@Service
+public class CreateJobUseCase {
+
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    public JobEntity execute(JobEntity jobEntity){
+        return this.jobRepository.save(jobEntity);
+
+
+
+
+
+    }
+
+
+
+
+
+}
