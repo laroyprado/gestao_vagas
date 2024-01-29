f0d237a459e0e24031f7a2211deadf8c03cb7068
laroy prado <laroyprado@gmail.com>
2024-01-29 20:22:49 -0300

    feat: Cria Entity de Company

---
+++ src/main/java/com/example/laroy/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -0,0 +1,15 @@
+package com.example.laroy.gestao_vagas.modules.company.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.laroy.gestao_vagas.modules.candidate.CandidateEntity;
+import com.example.laroy.gestao_vagas.modules.company.entities.CompanyEntity;
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
+
+    Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
+
+}
