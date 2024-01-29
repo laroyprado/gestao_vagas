b4af05ad493387a2951653f457f151f5d8ae7e72
laroy prado <laroyprado@gmail.com>
2024-01-29 20:23:15 -0300

    feat: cria use case de criação de company

---
+++ src/main/java/com/example/laroy/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -0,0 +1,28 @@
+package com.example.laroy.gestao_vagas.modules.company.useCases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.laroy.gestao_vagas.exceptions.UserFoundException;
+import com.example.laroy.gestao_vagas.modules.company.entities.CompanyEntity;
+import com.example.laroy.gestao_vagas.modules.company.repositories.CompanyRepository;
+
+
+@Service
+public class CreateCompanyUseCase {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    public CompanyEntity execute(CompanyEntity companyEntity) {
+
+        this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(),companyEntity.getEmail())
+        .ifPresent((user) ->{
+            throw new UserFoundException();
+        });
+
+
+        return this.companyRepository.save(companyEntity);
+    }
+    
+}
